c6fbc6dea23e35538e3a5a659aff48eb6eeca501
Arthur Muradyan <arthur.muradyan@yahoo.com>
2014-05-05 23:11:24 +0200

    using condition_variable instead of yield and sleep for waiting

--- streamer/src/main/jni/EventGenerator.cpp
+++ streamer/src/main/jni/EventGenerator.cpp
@@ -7,59 +7,60 @@
 #include <ctime>
 
 #include <thread>
-#include <chrono>
-#include <atomic>
+#include <mutex>
+#include <condition_variable>
 
 #define LOG_TAG "EventGeneratorJNI"
 #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
 #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
-// the callback from Java to call for triggering events
+/// the callback from Java to call for triggering events
 static jmethodID _event_callback;
 
-// Java VM 
+/// Java VM 
 static JavaVM* _java_vm = 0;
 
-// indicates whether event generator is stopped or not
-// since the variable is going to be read/modified from 
-// different threads we make changes atomic
-static std::atomic<bool> _stopped;
+/// indicates whether event generator is stopped or not
+static bool _stopped;
 
-// period for triggering event in milliseconds 
-static int _period = 800;
+/// synchronization primitives to block the thread 
+static std::mutex _mutex;
+static std::condition_variable _condition_variable;
 
-/// in a loop wait until either the period is elapsed or stopped becomes true
-static void wait()
+/// period for triggering event in milliseconds 
+static int _period = 500;
+
+/**
+ blocks the thread until either _period elapses or _stopped becomes true
+ returns true if _stopped became true
+ */
+static bool wait_until()
 {
-    const int epsilon = 50;
-    auto e = std::chrono::milliseconds(_period);
-    auto start = std::chrono::high_resolution_clock::now();
-    auto end = start + std::chrono::milliseconds(_period);
-    do {
-        std::this_thread::sleep_for(e);
-        // std::this_thread::yield();
-    } while (!_stopped.load() && std::chrono::high_resolution_clock::now() < end);
+    std::unique_lock<std::mutex> lck(_mutex);
+    auto a = std::chrono::duration<int,std::milli>(_period);
+    return _condition_variable.wait_for(lck, a, []{return _stopped;});
 }
 
 static void generate_random_events(JNIEnv* env, jobject thiz)
 {
     assert(nullptr != env);
-    while (!_stopped.load()) {
+    do {
         jstring msg = env->NewStringUTF("Event yeah");
         env->CallVoidMethod(thiz, _event_callback, rand() % 3, msg);
-        wait();
-    }
+    } while (!wait_until());
 }
 
-/// for local vs. global references 
-/// @see http://docs.oracle.com/javase/1.5.0/docs/guide/jni/spec/design.html#wp1242
-/// for more info about attaching & detaching to the thread
-/// @see http://developer.android.com/training/articles/perf-jni.html
+/**
+ for local vs. global references 
+ @see http://docs.oracle.com/javase/1.5.0/docs/guide/jni/spec/design.html#wp1242
+ for more info about attaching & detaching to the thread
+ @see http://developer.android.com/training/articles/perf-jni.html
+*/
 extern "C" JNIEXPORT void native_start(JNIEnv* env, jobject thiz) 
 {
     LOGI("native_start - begin");
-    _stopped.store(false);
+    _stopped = false;
     // need to get the global reference to share between threads
     jobject obj = env->NewGlobalRef(thiz);
     std::thread([obj]() {
@@ -70,7 +71,8 @@ extern "C" JNIEXPORT void native_start(JNIEnv* env, jobject thiz)
         int s1 = _java_vm->GetEnv((void**) &e, JNI_VERSION_1_4);
         if (s1 == JNI_EDETACHED) {
             int s2 = _java_vm->AttachCurrentThread((JNIEnv**) &e, nullptr);
-            LOGI(s2 != 0 ? "thread failed to attach" : "thread successfully attached");
+            LOGI(s2 != 0 ? "thread failed to attach" : 
+                           "thread successfully attached");
         } 
         generate_random_events(e, obj);
         e->DeleteGlobalRef(obj);
@@ -82,19 +84,21 @@ extern "C" JNIEXPORT void native_start(JNIEnv* env, jobject thiz)
 
 extern "C" JNIEXPORT void native_stop(JNIEnv* env, jobject thiz) 
 {
-    _stopped.store(true);
+    std::unique_lock<std::mutex> lck(_mutex);
+    _stopped = true;
+    _condition_variable.notify_one();
 }
 
-extern "C" JNIEXPORT jstring native_get_info(JNIEnv * env, jobject thiz) 
+extern "C" JNIEXPORT jstring native_get_info(JNIEnv* env, jobject thiz) 
 {
     return env->NewStringUTF("My Info");
 }
 
 /** List of implemented native methods */
 static JNINativeMethod native_methods[] = {
-    {"stopEvents", "()V", (void *) native_stop},
-    {"startEvents", "()V", (void *) native_start},
-    {"getInfo", "()Ljava/lang/String;", (void *) native_get_info},
+    {"stopEvents", "()V", (void*) native_stop},
+    {"startEvents", "()V", (void*) native_start},
+    {"getInfo", "()Ljava/lang/String;", (void*) native_get_info},
 };
 
 /** Library initializer */
@@ -107,7 +111,8 @@ jint JNI_OnLoad (JavaVM* vm, void* reserved)
         return 0;
     }
     jclass klass = env->FindClass("com/soundcloud/challenge/streamer/Streamer");
-    _event_callback = env->GetMethodID(klass, "onEvent","(ILjava/lang/String;)V");
+    _event_callback = env->GetMethodID(klass, "onEvent", 
+        "(ILjava/lang/String;)V");
     env->RegisterNatives(klass, native_methods, 3);
     srand(time(nullptr));
     return JNI_VERSION_1_4;
